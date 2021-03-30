785299fba2e68bf84f287242ec81291d9a179ec0
Antoine Simonot <73486863+AntoineSimonot@users.noreply.github.com>
2021-03-30 20:55:10 +0200

    test

--- public/index.php
+++ public/index.php
@@ -7,7 +7,7 @@ use Bramus\Router\Router;
 
 // Create Router instance
 $router = new Router();
-
+var_dump("test");
 
 $router->get('/account/login', '\App\Controller\UserController@showLogin');
 $router->post('/account/login', '\App\Controller\UserController@showLogin');
