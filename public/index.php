c79e5189be89dac5fcfa093cff1dceb835da4010
Antoine Simonot <simonotantoine1@gmail.com>
2021-03-30 19:04:08 +0200

    login

---
+++ public/index.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once __DIR__.'/../vendor/autoload.php';
+
+//use Framework\Router;
+use Bramus\Router\Router;
+
+// Create Router instance
+$router = new Router();
+
+
+$router->get('/account/login', '\App\Controller\UserController@showLogin');
+$router->post('/account/login', '\App\Controller\UserController@showLogin');
+$router->get('/account/registration', '\App\Controller\UserController@showRegistration');
+$router->post('/account/registration', '\App\Controller\UserController@showRegistration');
+$router->get('/account/disconnect', '\App\Controller\UserController@disconnectEvent');
+
+// $router->get('/test/{url}', '\App\Controller\UserController@testEvent');
+
+$router->run();
